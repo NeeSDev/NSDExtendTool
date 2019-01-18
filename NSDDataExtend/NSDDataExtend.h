d21b118dc57f9716fc963c806a6543b3ade9063e
倪巍 <niwei.develop@gmail.com>
2019-01-18 17:17:03 +0800

    beta 0.0.1.2

---
+++ NSDDataExtend/NSDDataExtend.h
@@ -0,0 +1,14 @@
+//
+//  NSDDataExtend.h
+//  NSDDataExtend
+//
+//  Created by NeeSDev on 2019/1/15.
+//  Copyright © 2019 NeeSDev. All rights reserved.
+//
+
+#ifndef NSDDataExtend_h
+#define NSDDataExtend_h
+
+#import "NSDKeychainAccess.h"
+
+#endif /* NeeSUIKitCategory_h */
---
+++ NSDExtendTool.h
@@ -0,0 +1,15 @@
+//
+//  NSDExtendTool.h
+//  NSDExtendTool
+//
+//  Created by NeeSDev on 2019/1/15.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#ifndef NSDExtendTool_h
+#define NSDExtendTool_h
+
+#import "NSDUIKitCategory.h"
+#import "NSDDataExtend.h"
+
+#endif /* NeeSUIKitCategory_h */
