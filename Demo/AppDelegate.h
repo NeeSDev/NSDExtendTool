a5e47596de2044727e5f20a495795d2b6c869baf
倪巍 <niwei.develop@gmail.com>
2019-01-18 18:09:46 +0800

    version 0.0.1

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSDExtendTools
+//
+//  Created by apple on 2019/1/18.
+//  Copyright © 2019 NeeSDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSDExtendTools
+//
+//  Created by apple on 2019/1/18.
+//  Copyright © 2019 NeeSDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
