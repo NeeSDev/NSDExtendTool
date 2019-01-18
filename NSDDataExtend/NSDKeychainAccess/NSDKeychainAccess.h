523cb2c4817a6a79d0de9d5ba2be3901ae4ef998
倪巍 <niwei.develop@gmail.com>
2019-01-18 17:04:00 +0800

    beta 0.0.1

---
+++ NSDDataExtend/NSDKeychainAccess/NSDKeychainAccess.h
@@ -0,0 +1,17 @@
+//
+//  KeychainAccess.h
+//  KeychainAccessSample
+//
+//  Created by Ravi Prakash on 17/11/14.
+//  Copyright (c) 2014 helpshift. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDKeychainAccess : NSObject
+- (void)nsd_KeychainSetValue:(id)data forKey:(NSString*)key;
+
+- (id)nsd_KeychainGetValueForKey:(NSString*)key;
+
+- (void)nsd_KeychainDeleteValueForKey:(NSString *)key;
+@end
---
+++ NSDDataExtend/NSDKeychainAccess/NSDKeychainAccess.m
@@ -0,0 +1,49 @@
+//
+//  KeychainAccess.m
+//  KeychainAccessSample
+//
+//  Created by Ravi Prakash on 17/11/14.
+//  Copyright (c) 2014 helpshift. All rights reserved.
+//
+
+#import "NSDKeychainAccess.h"
+
+@implementation NSDKeychainAccess
++ (NSMutableDictionary *)nsd_KeychainGetQuery:(NSString *)key {
+    NSMutableDictionary *querry =  [NSMutableDictionary dictionaryWithObjectsAndKeys:
+            (__bridge id)kSecClassGenericPassword, (__bridge id)kSecClass,
+            key, (__bridge id)kSecAttrAccount,
+            @"com.helpshift.test",(__bridge id)kSecAttrService,
+            nil];
+    
+    return  querry;
+}
+
+- (void)nsd_KeychainSetValue:(id)data forKey:(NSString*)key {
+    NSMutableDictionary *keychainQuery = [KeychainAccess nsd_KeychainGetQuery:key];
+    SecItemDelete((__bridge CFDictionaryRef)keychainQuery);
+    [keychainQuery setObject:[NSKeyedArchiver archivedDataWithRootObject:data] forKey:(__bridge id)kSecValueData];
+    SecItemAdd((__bridge CFDictionaryRef)keychainQuery, NULL);
+}
+
+- (id)nsd_KeychainGetValueForKey:(NSString *)key {
+    id value = nil;
+    NSMutableDictionary *keychainQuery = [KeychainAccess nsd_KeychainGetQuery:key];
+    //uncomment to see keychainQuery
+    //NSLog(@"%@",keychainQuery);
+    CFDataRef keyData = NULL;
+    [keychainQuery setObject:(__bridge id)kCFBooleanTrue forKey:(__bridge id)kSecReturnData];
+    [keychainQuery setObject:(__bridge id)kSecMatchLimitOne forKey:(__bridge id)kSecMatchLimit];
+    if (SecItemCopyMatching((__bridge CFDictionaryRef)keychainQuery, (CFTypeRef *)&keyData) == noErr) {
+            value = [NSKeyedUnarchiver unarchiveObjectWithData:(__bridge NSData *)keyData];
+    }
+    return value;
+}
+
+- (void)nsd_KeychainDeleteValueForKey:(NSString *)key {
+    NSMutableDictionary *keychainQuery = [KeychainAccess nsd_KeychainGetQuery:key];
+    SecItemDelete((__bridge CFDictionaryRef)keychainQuery);
+}
+
+
+@end
