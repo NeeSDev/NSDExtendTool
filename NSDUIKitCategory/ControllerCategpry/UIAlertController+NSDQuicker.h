2ed8d655842af540d07fb185af29e1b0febcbe8a
倪巍 <niwei.develop@gmail.com>
2019-01-23 16:20:48 +0800

    uialertcontroller 完善

--- NSDUIKitCategory/ControllerCategpry/UIAlertController+NSDQuicker.h
+++ NSDUIKitCategory/ControllerCategpry/UIAlertController+NSDQuicker.h
@@ -11,13 +11,37 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UIAlertController (NSDQuicker)
+
++(void)AlertWithTips:(NSString *)tips
+                 Tag:(__kindof UIViewController *)controller
+         ConfirmText:(NSString *)confirmText
+        ConfirmBlock:(nullable void(^)(void))confirmBlock;
+
 /**
  @brief 快速显示一个带有确定按钮的提示
-
+ 
  @param tips 提示文字
  @param controller 承载
  */
-+(void)AlertWithTips:(NSString *)tips Tag:(__kindof UIViewController *)controller;
++(void)AlertWithTips:(NSString *)tips
+                 Tag:(__kindof UIViewController *)controller;
+
++(void)AlertWithTips:(NSString *)tips
+                 Tag:(__kindof UIViewController *)controller
+        ConfirmBlock:(void(^)(void))confirmBlock;
+
++(void)AlertWithTips:(NSString *)tips
+                 Tag:(__kindof UIViewController *)controller
+         ConfirmText:(NSString *)confirmText
+          CancelText:(NSString *)cancelText
+        ConfirmBlock:(nullable void(^)(void))confirmBlock
+         CancelBlock:(nullable void(^)(void))cancelBlock;
+
++(void)AlertWithTips:(NSString *)tips
+                 Tag:(__kindof UIViewController *)controller
+        ConfirmBlock:(nullable void(^)(void))confirmBlock
+         CancelBlock:(nullable void(^)(void))cancelBlock;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- NSDUIKitCategory/ControllerCategpry/UIAlertController+NSDQuicker.m
+++ NSDUIKitCategory/ControllerCategpry/UIAlertController+NSDQuicker.m
@@ -9,11 +9,75 @@
 #import "UIAlertController+NSDQuicker.h"
 
 @implementation UIAlertController (NSDQuicker)
-+(void)AlertWithTips:(NSString *)tips Tag:(__kindof UIViewController *)controller
+
+
++(void)AlertWithTips:(NSString *)tips
+                 Tag:(__kindof UIViewController *)controller
+         ConfirmText:(NSString *)confirmText
+        ConfirmBlock:(void(^)(void))confirmBlock
 {
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:nil message:tips preferredStyle:UIAlertControllerStyleAlert];
-    [alert addAction:[UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleCancel handler:nil]];
-
+    [alert addAction:[UIAlertAction actionWithTitle:confirmText style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        if (confirmBlock) {
+            confirmBlock();
+        }
+    }]];
+    
     [controller presentViewController:alert animated:YES completion:nil];
+}
+
++(void)AlertWithTips:(NSString *)tips Tag:(__kindof UIViewController *)controller
+{
+    [UIAlertController AlertWithTips:tips
+                                 Tag:controller
+                         ConfirmText:@"确定"
+                        ConfirmBlock:nil];
+}
+
++(void)AlertWithTips:(NSString *)tips
+                 Tag:(__kindof UIViewController *)controller
+        ConfirmBlock:(void(^)(void))confirmBlock
+{
+    [UIAlertController AlertWithTips:tips
+                                 Tag:controller
+                         ConfirmText:@"确定"
+                        ConfirmBlock:confirmBlock];
+}
+
++(void)AlertWithTips:(NSString *)tips
+                 Tag:(__kindof UIViewController *)controller
+         ConfirmText:(NSString *)confirmText
+          CancelText:(NSString *)cancelText
+        ConfirmBlock:(void(^)(void))confirmBlock
+         CancelBlock:(void(^)(void))cancelBlock
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:nil message:tips preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:confirmText style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        if (confirmBlock) {
+            confirmBlock();
+        }
+    }]];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:cancelText style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        if (cancelBlock) {
+            cancelBlock();
+        }
+    }]];
+    
+    [controller presentViewController:alert animated:YES completion:nil];
+}
+
++(void)AlertWithTips:(NSString *)tips
+                 Tag:(__kindof UIViewController *)controller
+        ConfirmBlock:(void (^)(void))confirmBlock
+         CancelBlock:(void (^)(void))cancelBlock
+{
+    [UIAlertController AlertWithTips:tips
+                                 Tag:controller
+                         ConfirmText:@"确认"
+                          CancelText:@"取消"
+                        ConfirmBlock:confirmBlock
+                         CancelBlock:cancelBlock];
 }
 @end
